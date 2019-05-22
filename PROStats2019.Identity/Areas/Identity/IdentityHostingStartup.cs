9e4efcd38d0d5c904298936a736908898576a1c9
Per Orest <per.orest@outlook.com>
2019-05-22 11:15:31 +0200

    Deleted Identity due to issues with the IdentityUI

---
+++ PROStats2019.Identity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PROStats2019.Identity.Data;
+
+[assembly: HostingStartup(typeof(PROStats2019.Identity.Areas.Identity.IdentityHostingStartup))]
+namespace PROStats2019.Identity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
