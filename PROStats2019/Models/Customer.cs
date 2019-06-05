a3578d00de5234e2472b9247d9c6203660581d02
Per Orest <per.orest@outlook.com>
2019-06-05 11:40:22 +0200

    gfjdgj

---
+++ PROStats2019/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROStats2019.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string CustomerName { get; set; }
+    }
+}
